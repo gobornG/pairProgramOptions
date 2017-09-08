c72f854d4927d1fff67b3ed0e4c7043e7b1cb410
Garry <gtoborn@gmail.com>
2017-09-07 18:45:51 -0600

    added app.js file

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+
+
+class App extends Component {
+    render() {
+        return ( < //components here />
+        )
+    }
+}
+
+export default App;
